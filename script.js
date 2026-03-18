0b272843326be607377bd480239a1c1476aa5a0e
Guruprakash <p.guruprakash123456789@gmail.com>
2026-03-18 10:16:11 +0000

    todo app upgraded

--- script.js
+++ script.js
@@ -12,8 +12,15 @@ tasks.forEach((task, index) => {
 const li = document.createElement("li")
 
 li.innerHTML = `
-${task}
+<span style="text-decoration:${task.completed ? 'line-through' : 'none'}">
+${task.text}
+</span>
+
+<div>
+<button onclick="toggleTask(${index})">✔</button>
+<button onclick="editTask(${index})">✏</button>
 <button onclick="deleteTask(${index})">❌</button>
+</div>
 `
 
 list.appendChild(li)
@@ -26,7 +33,11 @@ const value = input.value.trim()
 
 if(value === "") return
 
-tasks.push(value)
+tasks.push({
+text: value,
+completed: false
+})
+
 input.value = ""
 
 saveTasks()
@@ -38,5 +49,30 @@ tasks.splice(index, 1)
 saveTasks()
 renderTasks()
 }
+
+function toggleTask(index){
+tasks[index].completed = !tasks[index].completed
+saveTasks()
+renderTasks()
+}
+
+function editTask(index){
+const newTask = prompt("Edit task:", tasks[index].text)
+
+if(newTask !== null && newTask.trim() !== ""){
+tasks[index].text = newTask.trim()
+saveTasks()
+renderTasks()
+}
+}
+
+/* ENTER KEY SUPPORT */
+
+document.getElementById("taskInput")
+.addEventListener("keydown", function(e){
+if(e.key === "Enter"){
+addTask()
+}
+})
 
 renderTasks()
